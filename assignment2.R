3e1175ad26174b4966dccf108654b31446738274
8959765 <8959765@163.com>
2020-02-13 18:16:25 +0800

    fisrt commit
    fisrt commit

---
+++ assignment2.R
@@ -0,0 +1,33 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+      m<-NULL
+      set<- function(u){
+        x<<-y
+        m<<-NULL
+}
+      get<-function() x
+      setInverse<-function(i) m<<-i
+
+      getInverse<- function() m
+      list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  inv<-x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(m)
+}
+  data<-x$get()
+  result<-solve(data)
+  x$setInverse(result)
+  result
+}
